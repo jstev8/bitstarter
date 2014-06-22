a90df373275a940b41863b88cf2a141b3ab6fb16
judy <judystev48@yahoo.com>
2014-06-21 21:32:43 -0400

    added index.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express();
 
+var file = fs.readFileSync(index.html);
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Welcome to Gitx')
+  response.send(file.toString('utf-8'));
 })
 
 app.listen(app.get('port'), function() {
