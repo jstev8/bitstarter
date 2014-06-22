bd63a79daad7bb8daec187c3f87ff3d3e3a371b0
judy <judystev48@yahoo.com>
2014-06-21 21:45:13 -0400

    modified js with index.html

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var file = fs.readFileSync(index.html);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+  response.send(file.toString('utf-8'));
 })
 
 app.listen(app.get('port'), function() {
