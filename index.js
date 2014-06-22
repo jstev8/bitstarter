940c83c0d4d56fec5ad9d88d29f7c29084da0fe6
judy <judystev48@yahoo.com>
2014-06-21 21:38:02 -0400

    back to HW2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express();
 
-var file = fs.readFileSync(index.html);
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(file.toString('utf-8'));
+  response.send('Hello World 2'));
 })
 
 app.listen(app.get('port'), function() {
