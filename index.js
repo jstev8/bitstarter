8443264b2f6d82e449d009174c2b6a93bc075638
judy <judystev48@yahoo.com>
2014-06-22 09:50:05 -0400

    added index.html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
 
+var data = fs.readFileSync("index.html", "utf-8");
 
 
 app.set('port', (process.env.PORT || 5000))
@@ -8,7 +10,7 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+  response.send(data);
 })
 
 app.listen(app.get('port'), function() {
